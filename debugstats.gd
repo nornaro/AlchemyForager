4a515bdcd48cb87b1d1f1ae49dfc904ef0a4ee2c
sugo Fenyő István <fuyuhimematsuri@gmail.com>
2024-09-19 04:11:20 +0200

    fight results update

--- debugstats.gd
+++ debugstats.gd
@@ -30,10 +30,32 @@ func _on_start_pressed() -> void:
 	var condition = 0
 	for i in 3:
 		var stat = partyStats.keys()[randi_range(0,4)]
-		print(stat)
 		if partyStats.get(stat) > level.get(stat):
-			condition += 1
+			condition += 1 # SUCCESS
 		if partyStats.get(stat) < level.get(stat):
-			condition -= 1
-	print(condition)
-	pass # Replace with function body.
+			condition -= 1 # FAIL Possible item loss
+	match condition:
+		0: text+="\nTie, \nNo negative effect"
+		-1: text+="\nDefeat, \n50% possible character loss"
+		-2: text+="\nDefeat, \nCharacter loss"
+		-3: text+="\nCritical defeat, \ncharacter loss, 25% possible team loss"
+		_: text+="\nVictory"
+
+
+func _on_predict_pressed() -> void:
+	var result = 0
+	for i in 9999:
+		result += mock_battle()
+	text += str(result/100)
+	
+func mock_battle() -> int:
+	var condition = 0
+	for i in 3:
+		var stat = partyStats.keys()[randi_range(0,4)]
+		if partyStats.get(stat) > level.get(stat):
+			condition += 1 # SUCCESS
+		if partyStats.get(stat) < level.get(stat):
+			condition -= 1 # FAIL Possible item loss
+	if condition > 0:
+		return 1
+	return 0
